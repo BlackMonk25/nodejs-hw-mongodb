2f08e6fe3fcfbecd2c93060566d4ee6378d82100
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-04 22:43:26 +0300

    final fix

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,6 +10,7 @@ import {
 export const getAllContactsCtrl = async (req, res) => {
   const contacts = await getAllContacts();
   res.status(200).json({
+     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -52,5 +53,8 @@ export const deleteContactCtrl = async (req, res) => {
   if (!deletedContact) {
     throw new createHttpError(404, 'Contact not found');
   }
-  res.status(204).end();
+  res.status(200).json({
+    status: 200,
+    message: `Contact with id ${id} deleted`,
+  });
 };
