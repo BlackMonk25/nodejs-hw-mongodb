75a333cd5e8651b0e71849b4da69c1eb03108642
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-04 23:15:48 +0300

    second fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,6 @@
+
 import createHttpError from 'http-errors';
 
-export const notFoundHandler = (res, req, next) => {
-  next(createHttpError.NotFound('Route not found'));
+export const notFoundHandler = (req, res, next) => {
+  next(createHttpError(404, 'Route not found'));
 };
