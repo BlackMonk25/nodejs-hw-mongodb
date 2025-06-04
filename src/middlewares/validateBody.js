e21d85415e906c53ad0a23719b0a75fd1c497643
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-05 00:10:10 +0300

    last fix

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,15 @@
+import createHttpError from 'http-errors';
+
+export const validateBody = (schema) => {
+  return async (req, resizeBy, next) => {
+    try {
+      await schema.validateAsync(req.body, {
+        abortEarly: false,
+      });
+      next();
+    } catch (error) {
+      const errors = error.details.map((detail) => detail.message);
+      next(createHttpError.BadRequest(errors));
+    }
+  };
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,21 +1,3 @@
-// import express from 'express';
-
-// import {
-//   createContactCtrl,
-//   deleteContactCtrl,
-//   getAllContactsCtrl,
-//   getContactByIdCtrl,
-//   updateContactCtrl,
-// } from '../controllers/contacts.js';
-// import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-// const router = express.Router();
-// router.get('/contacts', ctrlWrapper(getAllContactsCtrl));
-// router.get('/contacts/:id', ctrlWrapper(getContactByIdCtrl));
-// router.post('/contacts', ctrlWrapper(createContactCtrl));
-// router.patch('/contacts/:id', ctrlWrapper(updateContactCtrl));
-// router.delete('/contacts/:id', ctrlWrapper(deleteContactCtrl));
-// export default router;
-
 
 import express from 'express';
 
