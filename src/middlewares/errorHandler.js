3f847a126e4148d34667c5887f810e9a45b6eeb7
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-05 23:09:49 +0300

    first fix

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,19 @@
+
+
 import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  console.log('ErrorHandler:', err);
+  console.error('❌ ErrorHandler:', err);
 
   if (isHttpError(err)) {
-    res.status(err.status).json({
+    return res.status(err.status).json({
       status: err.status,
       message: err.message,
     });
   }
-  res.status(500).json({
+
+ 
+  return res.status(500).json({
     status: 500,
     message: 'Something went wrong',
     data: err.message,
