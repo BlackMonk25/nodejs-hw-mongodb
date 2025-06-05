b1f615566643acdde63eab5d87ddd5ab4af96322
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-05 20:15:12 +0300

    first push

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -10,3 +10,17 @@ export const getContactById = async (id) => {
   const contact = await Contacts.findById(id);
   return contact;
 };
+export const createContact = async (payload) => {
+  const contact = await Contacts.create(payload);
+  return contact;
+};
+export const updateContact = async (id, payload) => {
+  const updatedContact = await Contacts.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return updatedContact;
+};
+export const deleteContact = async (id) => {
+  const deletedContact = await Contacts.findByIdAndDelete(id);
+  return deletedContact;
+};
