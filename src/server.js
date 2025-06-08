06e12da8ce8ac2768f9a298b7ce77b91211b3f07
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-08 15:15:13 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,35 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import pino from 'pino-http';
-
-// import { getEnvVar } from './utils/getEnvVar.js';
-
-// import { notFoundHandler } from './middlewares/notFoundHandler.js';
-// import { errorHandler } from './middlewares/errorHandler.js';
-// import contactRoute from './routers/contacts.js';
-// const PORT = getEnvVar('PORT', '3000');
-// export const setupServer = () => {
-//   const app = express();
-//   app.use(express.json());
-//   app.use(cors());
-
-//   app.use(
-//     pino({
-//       transport: {
-//         target: 'pino-pretty',
-//       },
-//     }),
-//   );
-//   app.use(contactRoute);
-//   app.use(notFoundHandler);
-//   app.use(errorHandler);
-
-//   app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-//   });
-// };
-
-
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
