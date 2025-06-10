a6bd6c9606169538dabbc888b164f67b22eadd30
Plane-Jumper5 <planejumper2025@gmail.com>
2025-06-11 00:25:46 +0300

    add work

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,10 @@
 export const SORT_ORDER = {
-    ASC: 'asc',
-    DESC: 'desc',
+  ASC: 'asc',
+  DESC: 'desc',
 };
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const UPLOAD_DIR = 'uploads';
+export const TEMP_UPLOAD_DIR = 'temp';
   
--- src/server.js
+++ src/server.js
@@ -1,38 +1,35 @@
-import express from 'express';       
-import cors from 'cors';            
-import pinoHttp from 'pino-http';    
-import router from './routers/index.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import express from 'express';           
+import cors from 'cors';                
+import pinoHttp from 'pino-http';       
 import cookieParser from 'cookie-parser';
 
-
+import router from './routers/index.js'; 
+import { errorHandler } from './middlewares/errorHandler.js';      
+import { notFoundHandler } from './middlewares/notFoundHandler.js'; 
+import { UPLOAD_DIR } from './constants/index.js';                  
 
 export const setupServer = () => {
   const app = express(); 
 
-
-  app.use(cors());        
-  app.use(pinoHttp());    
+  app.use(cors()); 
+  app.use(pinoHttp()); 
   app.use(express.json()); 
-  app.use(cookieParser());
-  
+  app.use(cookieParser()); 
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
   app.use(router);
 
-  
-  
-app.use(notFoundHandler);  
-app.use(errorHandler);     
+  app.use(notFoundHandler); 
+  app.use(errorHandler);    
 
-
-  
   const PORT = process.env.PORT || 3000;
 
-  
   app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
   });
 };
+
 
 
 
